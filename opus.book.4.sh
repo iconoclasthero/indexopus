76a905b3e4554411f6f2c4cea76817224ed31852
iconoclast hero <iconoclasthero@gmail.com>
2024-05-02 20:36:15 -0400

    update

--- opus.book.4.sh
+++ opus.book.4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # nb: the swp file that editscript relies on is provided by nano
+# opusbook4ka is an external dependency
+
+trap '{ breakout; exit 1; }' INT
 shopt -s extglob
 scriptname="$(realpath $0)"
+script="${scriptname##*\/}"
 pidfile="/tmp/.opus.book.pids"
 threads=4
 mediaext="*.@(flac|mp3|MP3|wav|m4?|ogg|MP4|mp4|wma)"
@@ -12,43 +16,33 @@ editscript(){
      [[ ! -e "$swp" ]] && (/usr/bin/nano "$scriptname") && exit
      printf "\n%s is already being edited.\n%s exists; try fg or look in another window.\n" "$scriptname" "$swp"; exit ;}
 
-pause(){ read -p "$*" ; }
-
-threadno(){
-  filenum="$(ls ${mediaext}|wc -l)"
-  [[ "$filenum" -lt "$threads" ]] && threads="$filenum"
-}
-
+pause(){ read -rp "$*" ; }
 
 ffmpegs(){
-   ffmpeg -n -nostdin -hide_banner -loglevel error -stats -i "$1" -filter_complex "$filtercomplex" -c:a libopus -b:a 17k -frame_duration:a 60 "${1%.*}.opus"
-}
-
+   ffmpeg -n -nostdin -hide_banner -loglevel error -stats -i "$1" -filter_complex "$filtercomplex" -c:a libopus -b:a 17k -frame_duration:a 60 "${1%.*}.opus" ; }
 
 ffmpeg2(){
   mediafiles=( ${mediaext} )
-  for file in ${mediaext} #*.@(flac|mp3|MP3|wav|m4?|ogg|MP4|mp4|wma)
+  for file in ${mediaext}
     do
-       echo "${file%.*}" >> /tmp/mytmpfile
-#       echo "${file%.*}" |tee -a /tmp/mytmpfile
-       readarray -t ffoutput < <(ffmpeg -n -nostdin -hide_banner -loglevel error -stats -i "$file" -filter_complex "$filtercomplex" -c:a libopus -b:a 17k -frame_duration:a 60 -ar 24000 "${file%.*}.opus" 2>&1 >/dev/null)
-       erfile="${ffoutput[0]}"; erfile="${erfile%%=*}"
-       if [[ "$erfile" != "size" ]] && [[ "$erfile" != *already\ exists.\ Exiting* ]]
-         then
-           echo "${ffoutput[@]}"; echo "Error: exiting."; exit ;
-         else
-#           [[ "$erfile" != *already\ exists.\ Exiting* ]] && echo "$erfile"
-           y="$(sort /tmp/mytmpfile 2>/dev/null|uniq|wc -l)"
-           [[ "$y" -gt "$processed" ]] && processed="$y" &&
-              if [[ "${#mediafiles[@]}" -lt 100 ]]
-                then
-                  printf '%02d of %02d -- on %s\n' "$processed" "${#mediafiles[@]}" "$file"
-                else
-                  printf '%03d of %03d -- on %s\n' "$processed" "${#mediafiles[@]}" "$file"
-                fi
+      echo "${file%.*}" >> /tmp/mytmpfile
+      readarray -t ffoutput < <(ffmpeg -n -nostdin -hide_banner -loglevel error -stats -i "$file" -filter_complex "$filtercomplex" -c:a libopus -b:a 17k -frame_duration:a 60 -ar 24000 "${file%.*}.opus" 2>&1 >/dev/null)
+      erfile="${ffoutput[0]}"; erfile="${erfile%%=*}"
+      if [[ "$erfile" != "size" ]] && [[ "$erfile" != *already\ exists.\ Exiting* ]]
+        then
+          echo "${ffoutput[@]}"; echo "Error: exiting."; exit ;
+        else
+#         [[ "$erfile" != *already\ exists.\ Exiting* ]] && echo "$erfile"
+          y="$(sort /tmp/mytmpfile 2>/dev/null|uniq|wc -l)"
+          [[ "$y" -gt "$processed" ]] && processed="$y" &&                                 		# This does not work correctly and most likely never will
+            if [[ "${#mediafiles[@]}" -lt 100 ]]												# without a complete rewrite.  There are notes at the end about
+              then																				# wait and parallel, but at the end of the day, this is just
+                printf '%02d of %02d -- on %s\n' "$processed" "${#mediafiles[@]}" "$file"		# there to show that something is happening and progress is being
+              else																				# made so it's going to stay this way for a while!
+                printf '%03d of %03d -- on %s\n' "$processed" "${#mediafiles[@]}" "$file"		# Thu May  2 02:28:58 PM EDT 2024
+            fi
        fi
-    done
-}
+    done ; }
 
 
 stats(){
@@ -90,62 +84,38 @@ function breakout(){
    do
      [[ "$pid" = *"$script"* ]] || [[ "$pid" = *ffmpeg* ]] && echo "${pid% pts*}" >> "$pidfile"
   done< <(ps --tty $(tty) Sf)
-  opusbook4ka
-}
+  opusbook4ka "$pidfile" ; }
+
+# opusbook4ka is a dependency
 
 ###START
 
-rm /tmp/mytmpfile 2>/dev/null
-
 [[ "$1" == @(edit|e|nano|-e|-E) ]] && editscript
+rm /tmp/mytmpfile 2>/dev/null
+[[ -f "$pidfile" ]] && rm "$pidfile"
+startext=( ${mediaext} ); startext="${startext##*.}"
+filenum="$(ls ${mediaext}|wc -l)"
+[[ "$filenum" -lt "$threads" ]] && threads="$filenum"
 
-script="${0#*\/}"
+printf '%s\n\nDuration of %s %s file(s) to convert: %s\n\n\n' "$(pwd)" "$filenum" "$startext" "$(mediaduration "$startext")"
 
-if [ -f ".opus.book.pids" ]
- then
-  rm ".opus.book.pids"
-fi
-
-trap '{ breakout; exit 1; }' INT
-
-for g in ${mediaext};
- do
-  startext="${g#*.}"
- done
-
-threadno #function returns the number of threads to use
-
-echo
-printf '%s\nDuration of %s %s file(s) to convert: %s\n' "$(pwd)" "$filenum" "$startext" "$(mediaduration "$startext")"
-echo
-echo
-i=1
-while [[ "$i" -le "$threads" ]]
+for ((i=0; i<"$threads"; i++))
  do
   if [[ -n "$1" ]];
    then
      file="$1"
-#    ffmpegs "$1"&
      ffmpeg -n -nostdin -hide_banner -loglevel error -stats -i "$file" -filter_complex "$filtercomplex" -c:a libopus -b:a 17k -ar 24k -frame_duration:a 60 -ar 24000 "${file%.*}.opus"; exit
    else
-#    echo "Calling ffmpeg-$i"
     ffmpeg2&    # call function to background to convert media files to opus, counter
   fi
-   i=$(( "$i" + 1 ))
-   \sleep 0.5s
+   \sleep 0.5s  #jic i run from cli
  done
 
 wait  #for the bacground operations to finish up
 
 #stats   #not being used right now.  maybe make a cli option?  maybe not?
 
-if [ -f ".opus.book.pids" ]
- then
-  rm ".opus.book.pids"
- fi
-
-
-rm /tmp/mytmpfile 2>/dev/null
+rm /tmp/mytmpfile .opus.book.pids "$pidfile" 2>/dev/null
 
 exit
 
